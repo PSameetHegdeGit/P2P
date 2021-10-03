9324d4b81e82ef643dff841e4b893e4601bf00e3
PSameetHegdeGit <sameethegde123@gmail.com>
2021-10-03 15:22:36 -0500

    refactored server fxnality

--- src/ClientCommands/Broadcast.java
+++ src/ClientCommands/Broadcast.java
@@ -1,22 +1,28 @@
 package ClientCommands;
 
 import Interfaces.IClientCommand;
+import NodeResources.Client;
+import NodeResources.ThreadedMessageSender;
+import NodeResources.Tuple;
 import NodeTypes.Peer;
+
+import java.util.ArrayList;
 
 public class Broadcast implements IClientCommand {
 
-    private Peer p;
+    private Client _client;
 
 
-    public Broadcast(Peer p)
+    public Broadcast(Client client)
     {
-        this.p = p;
+        this._client = client;
     }
 
     @Override
     public void Execute()
     {
-
+        ArrayList<Tuple<String, Integer>> peersInNetwork = _client.peersInNetwork.Get();
+        peersInNetwork.forEach((peer_info) -> new Thread(new ThreadedMessageSender(_client, peer_info)).start());
     }
 
 
---
+++ src/Interfaces/IClientHandler.java
@@ -0,0 +1,13 @@
+package Interfaces;
+
+public interface IClientHandler {
+
+    void BroadcastHandler();
+
+    void MessageHandler(String message);
+
+    void Startup();
+
+    void GetArrayList();
+
+}
